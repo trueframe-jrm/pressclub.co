65e69501fd7681957055c1048989205025788994
TrueFrame <jvremivh@gmail.com>
2026-04-28 05:34:29 +0000

    fix supabase image loading

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+const { createClient } = window.supabase_js || supabase;
 
 const supabaseUrl = 'https://yitgtgwbphqvrjriroqg.supabase.co'
 const supabaseKey = 'sb_publishable_yCqdjTJans7FmjbxiOHdSA_T96Kdrq3'
